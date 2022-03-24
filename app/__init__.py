6b929454e318c1844b41139b59f12ede89b665b2
Artem Abramov <notdepot@gmail.com>
2022-03-24 20:03:24 +0000

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
+from .config import Config
 from flask import Flask
 import connexion
 
 connexion_app = connexion.App(__name__, specification_dir='./')
 connexion_app.add_api('swagger.yml')
-app = connexion_app.app
 
-#flask_app = Flask(__name__)
-#@flask_app.route('/hello')
-#def hello():
-#    return 'hello'
+app = connexion_app.app
+app.config.from_object(Config)
+
+tmp = 1
+
+@app.before_first_request
+def create_tables():
+    #db.create_all()
+    pass
+
+@app.before_request
+def asdfasfd():
+    pass
---
+++ app/routes/hello.py
@@ -0,0 +1,6 @@
+from flask import request
+from app import tmp
+
+def hi():
+    a = 1
+    return 'hi!'
