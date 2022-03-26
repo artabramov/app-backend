296660ad2c7d98c04da92369078a2a35493b24aa
Artem Abramov <notdepot@gmail.com>
2022-03-26 10:59:53 +0000

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,9 @@
 from .config import Config
 from flask import Flask
-
-
-#from core.connexion import ConnexionApp
-#connexion_app = ConnexionApp(__name__, specification_dir='./')
-#connexion_app.creare_api('swagger.yml', api_name='asdads')
-
-"""
 import connexion
+
 connexion_app = connexion.FlaskApp(__name__, specification_dir='./')
 connexion_app.add_api('swagger.yml')
 
 app = connexion_app.app
 app.config.from_object(Config)
-
-@app.before_request
-def before_request():
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-"""
-
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-@app.before_request
-def before_request():
-    print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-
-@app.route('/hi')
-def qwer():
-    return 'flask!'
-
-
-a = 1
--- app/routes/hello.py
+++ app/routes/hello.py
@@ -1,11 +1,4 @@
-from flask import request
-
-
 def hi():
-    from app import a
-    #from app import log
-
-    #log.debug('hi')
-    return 'hi2'
+    return 'hi!'
 
 
