4ba49ceb7e5be26604a9e154a6f8a6d39b010692
Artem Abramov <notdepot@gmail.com>
2022-03-26 15:32:45 +0000

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from .config import Config
 from flask import Flask
+from app.core.logger import logger
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+log = logger(app)
 
 from app.routes.hello import hi
 
--- app/routes/hello.py
+++ app/routes/hello.py
@@ -1,9 +1,10 @@
 from flask import jsonify
-from app import app
+from app import app, log
 
 
 @app.route('/hi/')
 def hi():
+    log.error('hi error')
     return jsonify({"hello": "hi!"})
 
 
