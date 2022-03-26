7b1d8f5093e267a34208dd7f84a70eae262cd04c
Artem Abramov <notdepot@gmail.com>
2022-03-26 11:40:45 +0000

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,3 @@
-"""
-from .config import Config
-from flask import Flask
-import connexion
-
-connexion_app = connexion.FlaskApp(__name__, specification_dir='./')
-connexion_app.add_api('swagger.yml')
-
-app = connexion_app.app
-app.config.from_object(Config)
-"""
-
 from .config import Config
 from flask import Flask
 
@@ -17,6 +5,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route('/hi')
+#@swag_from('swagger.yml')
 def hi():
     return 'hi2!'
 
