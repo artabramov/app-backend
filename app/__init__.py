4c7587b3a31b60d48502e9447f727977b166f0ec
Artem Abramov <notdepot@gmail.com>
2022-03-27 12:14:44 +0000

    tmp

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from .config import Config
 from flask import Flask, request
-from app.core.create_logger import create_logger
+#from app.core.create_logger import create_logger
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-log = create_logger(app)
+#log = create_logger(app)
 
 from app.routes.hello import hi
 
