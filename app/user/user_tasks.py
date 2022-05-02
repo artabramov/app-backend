09d57cd0df8c588cf09e0057d572ec7e8c27d367
Artem Abramov <notdepot@gmail.com>
2022-05-02 19:36:44 +0000

    fix

--- app/user/user_tasks.py
+++ app/user/user_tasks.py
@@ -32,7 +32,7 @@ def user_register(user_login, user_name, user_pass):
         cache.set('user.%s' % (user.id), user)
         import qrcode
         img = qrcode.make(user.code_secret)
-        img.save('/app/qrs/%s.png' % user.code_secret)
+        img.save('/app/qr/%s.png' % user.code_secret)
         return {'code_secret': user.code_secret}, {}, 201
 
     except ValidationError as e:
