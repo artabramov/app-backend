9dd52628bffaffdcc622c3f8b25f7b30a384dea9
Artem Abramov <49606840+artabramov@users.noreply.github.com>
2022-06-26 10:10:16 +0000

    tmp

--- app/hi/hi_routes.py
+++ app/hi/hi_routes.py
@@ -2,11 +2,11 @@ from app.core.app_response import app_response
 from app import app
 
 
-@app.route('/api/hi')
+@app.route('/hi')
 @app_response
-def hi():
+def hi1():
     return {
-        'hello': 'hi!',
+        'hello': 'hi1',
         'token': 'e9456129-1ead-44cf-aa8c-d49c2719e508',
         'user': {
             'Token': 'e9456129-1ead-44cf-aa8c-d49c2719e508'
@@ -14,3 +14,13 @@ def hi():
     }, {}, 200
 
 
+@app.route('/api/hi')
+@app_response
+def hi2():
+    return {
+        'hello': 'hi2',
+        'token': 'e9456129-1ead-44cf-aa8c-d49c2719e508',
+        'user': {
+            'Token': 'e9456129-1ead-44cf-aa8c-d49c2719e508'
+        }
+    }, {}, 200
